6b021b009d311d6e475f96148bb4284962b3e820
Asmandiyar Jumaliev <jasmandiyar@gmail.com>
2023-12-02 16:19:20 +0600

    Добавьте файлы проекта.

---
+++ exam 1 my/Device.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_1_my
+{
+    // Базовый класс для устройств
+    internal  class Device
+    {
+        public string Name { get; set; }
+        public bool IsOn { get; set; }
+
+        public Device(string name)
+        {
+            Name = name;
+            IsOn = false;
+        }
+
+        public void TurnOn()
+        {
+            IsOn = true;
+            Console.WriteLine($"{Name} включено");
+        }
+
+        public void TurnOff()
+        {
+            IsOn = false;
+            Console.WriteLine($"{Name} выключено");
+        }
+    }
+}
---
+++ exam 1 my/Lamp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_1_my
+{
+    // Класс для лампы, наследует от базового класса устройства
+    internal class Lamp : Device
+    {
+        public int Brightness { get; set; }
+
+        public Lamp(string name) : base(name)
+        {
+            Brightness = 50; // Начальная яркость
+        }
+
+        public void AdjustBrightness(int newBrightness)
+        {
+            // Убеждаемся, что яркость находится в пределах от 0 до 100
+            Brightness = Math.Max(0, Math.Min(100, newBrightness));
+            Console.WriteLine($"{Name} яркость установлена на {Brightness}%");
+        }
+    }
+}
